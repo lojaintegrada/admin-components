27cd768331d4d6b9d5d2951f0752cbcf397cee27
Gustavo Rosolem <guspace92@hotmail.com>
2021-05-19 15:31:06 -0300

    Add styleguide folder

---
+++ styleguide/src/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import { Sidebar } from './index';
+
+export default {
+  title: 'Example/Sidebar',
+  component: Sidebar,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+export const Default: Story = () => {
+  return (
+    <>
+      <Sidebar />
+      <link rel="stylesheet" href="https://cdn.awsli.com.br/qa1/static-v2/painel/css/admin.css?v=c2e27d9" />
+    </>
+  )
+}
