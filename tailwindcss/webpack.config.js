460e35ec8074fe7016aed5c0f5cfa41f47e38e06
Gustavo Rosolem <guspace92@hotmail.com>
2021-05-21 10:49:47 -0300

    Add tailwindconfig (#3)

---
+++ tailwindcss/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: {
+    defaultSettings: './src/defaultSettings.js',
+    pagaLISettings: './src/pagaLISettings.js',
+  },
+  output: {
+    filename: '[name].js'
+  }
+};
