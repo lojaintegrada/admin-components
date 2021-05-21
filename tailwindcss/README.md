17b905d816880dd2618626646d048ddd1634e978
Gustavo Rosolem <guspace92@hotmail.com>
2021-05-21 10:49:08 -0300

    Add tailwindconfig

---
+++ tailwindcss/README.md
@@ -0,0 +1,23 @@
+# Loja Integrada Admin TailwindCSS Config
+
+## Use
+
+tailwind.config.js
+
+```js
+module.exports = {
+    theme: {
+        ...
+    },
+    plugins: [
+        require('@lojaintegrada/tailwindcss-config/dist/defaultSettings.js'),
+    ]
+};
+```
+
+## Commands
+
+```bash
+yarn build
+npm publish
+```
---
+++ tailwindcss/src/pagaLISettings.js
@@ -0,0 +1,74 @@
+import * as defaultSettings from './defaultSettings'
+
+module.exports = {
+  ...defaultSettings,
+  config: {
+    theme: {
+      colors: {
+        'current': 'currentColor',
+        'transparent': 'transparent',
+        'black-alpha': 'rgba(0,0,0,.25)',
+
+        'base-1': '#000000',
+        'base-2': '#F8F8F9',
+        'base-3': '#E8E9EB',
+        'base-4': '#D5D7DA',
+
+        'inverted-1': '#20221B',
+        'inverted-2': '#83888E',
+        'inverted-3': '#E8E9EB',
+        'inverted-4': '#F8F8F9',
+
+        'on-primary': '#FFFFFF',
+        'on-sucess': '#FFFFFF',
+        'on-danger': '#FFFFFF',
+        'on-warning': '#FFFFFF',
+
+        'on-inverted': '#FFFFFF',
+        'on-base': '#142032',
+        'on-base-2': '#7C828A',
+        'on-base-3': '#D0D2D6',
+
+        'primary-light': '#F2F8E8',
+        'primary': '#9CCB3B',
+        'primary-dark': '#86B623',
+        'primary-bold': '#749335',
+
+        'success-light': '#F3F8E8',
+        'success': '#9CCB3B',
+        'success-dark': '#86B623',
+        'success-bold': '#749335',
+
+        'danger-light': '#FCF1EE',
+        'danger': '#DD4A2C',
+        'danger-dark': '#C43F24',
+        'danger-bold': '#AA341C',
+
+        'warning-light': '#FBF5E5',
+        'warning': '#FFC20E',
+        'warning-dark': '#D99D09',
+        'warning-bold': '#B37B06',
+
+        'secondary-light': '#F4FAFD',
+        'secondary': '#B9DEF0',
+        'secondary-dark': '#5BA2C4',
+        'secondary-bold': '#5BA2C4',
+
+        'orange-promax': '#FF4007',
+
+        'google': '#EA4335',
+        'google-dark': '#D0382C',
+        'google-bold': '#B42E23',
+        'google-red': '#FF4C4C',
+        'google-blue': '#368DF7',
+        'google-purple': '#8914CC',
+        'google-green': '#8BC34A',
+        'google-yellow': '#F4B400',
+
+        'card-stroke': '#CED4D8',
+        'card-stroke-2': '#A3AAB5',
+        'card-shadow': '#E1E4E7'
+      }
+    }
+  }
+}
