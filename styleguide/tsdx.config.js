c720dea1c067a37250ac0634066659a8563532b7
Gustavo Rosolem <guspace92@hotmail.com>
2021-05-24 19:37:22 -0300

    Add import css to each component

--- styleguide/tsdx.config.js
+++ styleguide/tsdx.config.js
@@ -1,24 +1,21 @@
 const path = require('path')
 const postcss = require('rollup-plugin-postcss');
-const extractSelectors = require('./scripts/extract-selectors')
 
 module.exports = {
   rollup(config, options) {
-    if(!!options.writeMeta) {
-      config.plugins.push(
-        postcss({
-          config: {
-            path: './postcss.config.js',
-          },
-          extensions: ['.css'],
-          minimize: true,
-          inject: {
-            insertAt: 'top',
-          },
-          extract: path.resolve('dist/tailwind.css'),
-        })
-      );
-    }
+    config.plugins.push(
+      postcss({
+        config: {
+          path: './postcss.config.js',
+        },
+        extensions: ['.css'],
+        minimize: true,
+        // inject: {
+        //   insertAt: 'top',
+        // },
+        extract: !!options.writeMeta ? path.resolve('dist/tailwind.css') : false,
+      })
+    );
     return config;
   },
 };
