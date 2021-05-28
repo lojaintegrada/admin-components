18bc75e3d410544b2a195cb259b66c8a8745b4ca
Gustavo Rosolem <guspace92@hotmail.com>
2021-05-28 17:04:52 -0300

    Add button component

---
+++ styleguide/src/Components/Button/Button.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import { Button } from './Button'
+
+describe('Button tests', () => {
+  it('should have overridable styles', () => {
+    const { container } = render(
+      <Button data-testid="button">
+        Black Button
+      </Button>
+    )
+
+    expect(container).toBeDefined()
+  })
+
+  it('should not have a11y violations', async () => {
+    const { container } = render(
+      <>
+        <Button>Button</Button>
+        <Button>Button</Button>
+        <Button variant="secondary">Button</Button>
+        <Button variant="tertiary">Button</Button>
+        <Button size="small">Button</Button>
+        <Button variant="danger">Button</Button>
+      </>
+    )
+
+    expect(container).toBeDefined()
+  })
+})
--- styleguide/src/Components/Button/Button.tsx
+++ styleguide/src/Components/Button/Button.tsx
@@ -6,11 +6,38 @@ import {
 } from 'reakit'
 import './../../tailwind.css'
 
+const listOfStylesHover = {
+  primary: `hover:bg-primary-dark`,
+  secondary: `hover:bg-primary-light`,
+  tertiary: `hover:bg-tertiary-dark`,
+  info: `hover:bg-secondary-bold`,
+  warning: `hover:bg-warning-dark`,
+  danger: `hover:bg-danger-dark`,
+}
+const listOfStylesActive = {
+  primary: `active:bg-primary-bold`,
+  secondary: `active:bg-primary-light`,
+  tertiary: `active:bg-tertiary-bold`,
+  warning: `active:bg-warning-bold`,
+  danger: `hover:bg-danger-bold`,
+}
+const listOfStylesFocus = {
+  primary: `focus:ring-1 focus:ring-primary-dark focus:ring-opacity-50`,
+  danger: `focus:ring-1 focus:ring-danger-dark`,
+}
 const listOfStyles = {
-  primary: `bg-primary text-base-1 `,
-  secondary: `bg-transparent text-primary ring-1 ring-primary ring-inset `,
-  tertiary: `bg-inverted-2 text-on-primary `,
-  danger: `ba b--danger bg-danger white hover-bg-danger-dark hover-b--danger-dark   pointer `
+  primary: `bg-primary text-base-1 ${listOfStylesHover['primary']} ${listOfStylesActive['primary']} ${listOfStylesFocus['primary']}`,
+  secondary: `bg-transparent text-primary ring-1 ring-primary ring-inset ${listOfStylesHover['secondary']} ${listOfStylesActive['secondary']}`,
+  tertiary: `bg-inverted-2 text-on-primary ${listOfStylesHover['tertiary']} ${listOfStylesActive['tertiary']}`,
+  info: `bg-secondary-dark text-base-1 ${listOfStylesHover['info']}`,
+  warning: `bg-warning text-on-base ${listOfStylesHover['warning']} ${listOfStylesActive['warning']}`,
+  danger: `bg-danger text-base-1 ${listOfStylesHover['danger']} ${listOfStylesActive['danger']} ${listOfStylesFocus['danger']}`,
+}
+
+const listOfSizes = {
+  small: `text-f7 h-8`,
+  default: `text-f6 h-12`,
+  large: `text-f5 h-14`,
 }
 
 export const Button = React.memo(
@@ -23,19 +50,28 @@ export const Button = React.memo(
     disabled,
     id,
     className,
-    onClick
+    onClick,
+    size
   }: ButtonProps) => {
+
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
       !disabled && onClick && onClick(event)
     }
 
-    if (!variant) variant = 'primary'
-    let classes = `flex font-semibold items-center justify-center px-5 text-center no-underline transition `
-    classes += disabled
-      ? 'bg-base-3 cursor-default text-on-base-2 shadow-none ring-0 border-0 '
-      : `${listOfStyles[variant]} `
-    if (fullWidth) classes += 'w-100 '
-    if (className) classes += className
+    if(!variant) variant = 'primary'
+    if(!size) size = 'default'
+
+    let classes = `flex font-semibold tracking-tight items-center justify-center px-5 text-center no-underline transition rounded after:align-middle focus:outline-none `
+
+    if(disabled) {
+      classes += 'bg-base-3 cursor-default text-on-base-2 shadow-none ring-0 border-0 hover:bg-base-3 hover:text-on-base-2'
+    } else {
+      classes += `${listOfStyles[variant]} `
+    }
+    classes += `${listOfSizes[size]} `
+
+    if(fullWidth) classes += 'w-full '
+    if(className) classes += className
 
     return (
       <Provider>
@@ -55,19 +91,19 @@ export const Button = React.memo(
 
 export interface ButtonProps extends ReakitButtonProps {
   /** Size of the button
-   * @default regular
+   * @default default
    * */
-  size?: 'regular' | 'large' | 'small'
+  size?: 'default' | 'large' | 'small'
   /** Button variant
    * @default primary
    * */
-  variant?: 'primary' | 'secondary' | 'tertiary'
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'info' | 'warning' | 'danger'
   /** Button status
    * @default null
    * */
   status?: 'loading' | 'success' | 'error'
-  /**
-   * Make button full width
+  /** Make button full width
+   * @default false
    */
   fullWidth?: boolean
   /**
