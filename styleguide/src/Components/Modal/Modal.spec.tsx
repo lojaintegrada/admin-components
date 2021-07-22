fcf3651c1f2de979a5c9eefa09b73b3b3b458eb7
Gustavo Rosolem <guspace92@hotmail.com>
2021-07-22 17:08:53 -0300

    Remove updateArgs due to incompatibility with Cypress test

--- styleguide/src/Components/Modal/Modal.spec.tsx
+++ styleguide/src/Components/Modal/Modal.spec.tsx
@@ -9,7 +9,6 @@ describe('Modal tests', () => {
 
   it('Default', () => {
     mount(<Default />)
-    
   })
 
 })
--- styleguide/src/Components/Modal/Modal.stories.tsx
+++ styleguide/src/Components/Modal/Modal.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react'
-import { useArgs } from '@storybook/client-api'
 
 import { Modal, ModalProps } from './Modal'
 import { Box } from '../../Layout/Box'
@@ -18,36 +17,30 @@ export default {
   },
 } as Meta
 
-const Template: Story<ModalProps> = args => {
-  const [_, updateArgs] = useArgs()
-  const handleClose = () => {
-    updateArgs({ isOpen: false })
-  }
-  return (
-    <div>
-      <Modal {...args} onClose={handleClose} />
-      <Box>
-        <Box.Content>
-          Any page content
-        </Box.Content>
-      </Box>
-      <Box>
-        <Box.Content>
-          Any page content
-        </Box.Content>
-      </Box>
-      <Box>
-        <Box.Content>
-          Any page content
-        </Box.Content>
-      </Box>
-      <Box>
-        <Box.Content>
-          Any page content
-        </Box.Content>
-      </Box>
-    </div>
-  )
-}
+const Template: Story<ModalProps> = args => (
+  <div>
+    <Modal {...args} />
+    <Box>
+      <Box.Content>
+        Any page content
+      </Box.Content>
+    </Box>
+    <Box>
+      <Box.Content>
+        Any page content
+      </Box.Content>
+    </Box>
+    <Box>
+      <Box.Content>
+        Any page content
+      </Box.Content>
+    </Box>
+    <Box>
+      <Box.Content>
+        Any page content
+      </Box.Content>
+    </Box>
+  </div>
+)
 
 export const Default = Template.bind({})
