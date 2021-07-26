043b7221193237cdbe23162cdcd04ad4a8722bb9
Gustavo Rosolem <guspace92@hotmail.com>
2021-07-26 14:51:12 -0300

    Add angle right icon

---
+++ styleguide/src/Icons/icons-path/AngleRight.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AngleRight = () => (
+  <path
+    fillRule="evenodd"
+    d="M7.25 14a1 1 0 01-.665-1.748L10.245 9l-3.66-3.253a1 1 0 111.33-1.495l4.5 4a1.003 1.003 0 010 1.495l-4.5 4A1 1 0 017.25 14z"
+    clipRule="evenodd"
+  />
+)
