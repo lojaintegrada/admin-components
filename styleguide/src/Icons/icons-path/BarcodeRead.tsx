1392bec0b2873fdd9184abf06ba1160f18675d7b
Gustavo Rosolem <guspace92@hotmail.com>
2021-07-30 17:37:09 -0300

    Added icons: barcode, check, clock, times

---
+++ styleguide/src/Icons/icons-path/BarcodeRead.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BarcodeRead = () => (
+  <path
+    fillRule="evenodd"
+    d="M13 12h1V6h-1v6zm-3 0h2V6h-2v6zm-2.5 0h1V6h-1v6zM4 12h2V6H4v6zm12.5-1.5v3c0 .552-.447 1-1 1h-3V16h3c1.381 0 2.5-1.119 2.5-2.5v-3h-1.5zm-11 4h-3c-.553 0-1-.448-1-1v-3H0v3C0 14.881 1.119 16 2.5 16h3v-1.5zm12.5-7v-3C18 3.119 16.881 2 15.5 2h-3v1.5h3c.553 0 1 .448 1 1v3H18zm-12.5-4h-3c-.553 0-1 .448-1 1v3H0v-3C0 3.119 1.119 2 2.5 2h3v1.5z"
+    clipRule="evenodd"
+  />
+)
