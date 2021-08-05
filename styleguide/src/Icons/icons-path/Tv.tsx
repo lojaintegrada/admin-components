e75bcccc3223b6025376e0f1c2412afe2f7a9392
Gustavo Rosolem <guspace92@hotmail.com>
2021-08-05 15:42:03 -0300

    Add new icons for sidebar

---
+++ styleguide/src/Icons/icons-path/Tv.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Tv = () => (
+  <path
+    fillRule="evenodd"
+    d="M1.5 12h15V3h-15v9zm15-10.5h-15A1.5 1.5 0 000 3v9a1.5 1.5 0 001.5 1.5h6.75V15H6a1.5 1.5 0 00-1.5 1.5h9A1.5 1.5 0 0012 15H9.75v-1.5h6.75A1.5 1.5 0 0018 12V3a1.5 1.5 0 00-1.5-1.5z"
+    clipRule="evenodd"
+  />
+)
