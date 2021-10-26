0a03414c394a4d8bb2a3e8b0e4664b61abe24c6e
Natanael Fernandes <natan-fernandes1@hotmail.com>
2021-10-26 14:50:12 -0300

    feat: add formatValue onlyText to inputmask

--- styleguide/src/Forms/InputMask/utils.ts
+++ styleguide/src/Forms/InputMask/utils.ts
@@ -11,14 +11,20 @@ export const formatValuePatterns = {
   },
   onlyLetter: {
     mask: (rawValue: any) => {
-      const onlyAlphanumeric = rawValue.replace(/[^A-zÀ-ú]/g, '').split('')
-      return onlyAlphanumeric.map(() => /[A-zÀ-ú]/)
+      const onlyLetter = rawValue.replace(/[^A-zÀ-ú]/g, '').split('')
+      return onlyLetter.map(() => /[A-zÀ-ú]/)
     },
   },
   onlyAlphanumeric: {
     mask: (rawValue: any) => {
       const onlyAlphanumeric = rawValue.replace(/\W/g, '').split('')
       return onlyAlphanumeric.map(() => /\w/)
+    },
+  },
+  onlyText: {
+    mask: (rawValue: any) => {
+      const onlyText = rawValue.replace(/[^a-zA-ZÀ-ú'~0-9\s]/g, '').split('')
+      return onlyText.map(() => /[a-zA-ZÀ-ú'~´`^0-9\s]/g)
     },
   },
   date: {
