dd63a0cba9a36174b82de25ae16199190588e148
Natanael Fernandes <natan-fernandes1@hotmail.com>
2021-11-10 15:25:51 -0300

    feat: adding angles diagonal, width and height icons

---
+++ styleguide/src/Icons/icons-path/AngleDiagonal.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const AngleDiagonal = () => (
+  <>
+    <path
+      d="M12.7122 4.22709L13.7728 5.28775L5.28753 13.773L4.22687 12.7124L12.7122 4.22709Z"
+      fillRule="evenodd"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M13.8425 4.15742V8.75742H12.6425L12.6425 5.35742L9.24248 5.35742V4.15742L13.8425 4.15742Z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M4.1572 13.8427H8.7572L8.7572 12.6427L5.3572 12.6427L5.3572 9.2427L4.1572 9.2427V13.8427Z"
+    />
+  </>
+)
