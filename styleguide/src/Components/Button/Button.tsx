198ff02eaa55b612b73753f63b1cf1412c6cc2a2
Fernando Pavanello <fernando.pavanello2@gmail.com>
2022-01-05 10:50:53 -0300

    feat: add only text button variation

--- styleguide/src/Components/Button/Button.tsx
+++ styleguide/src/Components/Button/Button.tsx
@@ -18,12 +18,14 @@ const listOfStylesActive = {
   warning: `active:bg-warning-bold`,
   danger: `hover:bg-danger-bold`,
   outline: `active:shadow-inner active:bg-base-1`,
+  onlyText: `active:bg-base-1`,
 }
 const listOfStylesFocus = {
   primary: `focus:ring-1 focus:ring-primary-dark focus:ring-opacity-50`,
   secondary: `focus:ring focus:ring-focus`,
   danger: `focus:ring-1 focus:ring-danger-dark`,
   outline: `focus:ring-2 focus:ring-focus focus:ring-offset-1`,
+  onlyText: `focus:bg-base-1`,
 }
 const listOfStyles = {
   primary: `bg-primary text-base-1 ${listOfStylesHover['primary']} ${listOfStylesActive['primary']} ${listOfStylesFocus['primary']}`,
@@ -33,6 +35,7 @@ const listOfStyles = {
   warning: `bg-warning text-on-base ${listOfStylesHover['warning']} ${listOfStylesActive['warning']}`,
   danger: `bg-danger text-base-1 ${listOfStylesHover['danger']} ${listOfStylesActive['danger']} ${listOfStylesFocus['danger']}`,
   outline: `bg-transparent text-inverted-2 border border-inverted-2 ${listOfStylesHover['outline']} ${listOfStylesActive['outline']} ${listOfStylesFocus['outline']}`,
+  onlyText: `bg-transparent border-transparent text-inverted-2 px-0 ${listOfStylesActive['onlyText']} ${listOfStylesFocus['onlyText']}`,
 }
 
 const defaultDisabledStyle = `bg-base-3 cursor-default text-on-base-2 shadow-none ring-0 border-0 hover:bg-base-3 hover:text-on-base-2 `
@@ -44,6 +47,7 @@ const listOfStylesDisabled = {
   warning: defaultDisabledStyle,
   danger: defaultDisabledStyle,
   outline: defaultDisabledStyle,
+  onlyText: defaultDisabledStyle,
 }
 
 const listOfSizes = {
@@ -150,6 +154,7 @@ export interface ButtonProps extends ButtonAnchorProps {
     | 'warning'
     | 'danger'
     | 'outline'
+    | 'onlyText'
   /**
    * Button is loading
    * */
