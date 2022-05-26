bd4df69a5c2bb1bdd97e10d158deeb8c1d1b091f
Gustavo Rosolem <guspace92@hotmail.com>
2022-05-26 18:57:35 -0300

    Update pagali color

--- tailwindcss/src/pagaLIPreset.js
+++ tailwindcss/src/pagaLIPreset.js
@@ -1,8 +1,7 @@
 const defaultPreset = require('./defaultPreset')
 
 // Change color for PagaLI
-defaultPreset.theme.colors.primary = "#F2633A"
-defaultPreset.theme.colors['inverted-1'] = "#1C2D42"
-defaultPreset.theme.colors['on-base'] = "#1C2D42"
+defaultPreset.theme.colors['inverted-1'] = "#004852"
+defaultPreset.theme.colors['on-base'] = "#004852"
 
 module.exports = defaultPreset
