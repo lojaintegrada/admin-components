bb060efe81e26d3ebe581e5b617012eecef258a0
Arian Carvalho <carvalhobva@gmail.com>
2022-08-16 17:52:22 -0300

    lint pattern

--- styleguide/src/Indicators/InformationBox/InformationBox.tsx
+++ styleguide/src/Indicators/InformationBox/InformationBox.tsx
@@ -12,20 +12,20 @@ const InformationBoxTypes: Record<
     iconClass: string
   }
 > = {
-    tips: {
-    title:"Dica!",
+  tips: {
+    title: 'Dica!',
     class: 'bg-success-light border-success',
     icon: 'lightbulb',
     iconClass: 'text-success',
   },
   warning: {
-    title:"Atenção!",
+    title: 'Atenção!',
     class: 'bg-warning-light border-warning',
     icon: 'exclamationTriangle',
     iconClass: 'text-warning',
   },
   danger: {
-    title:"Cuidado!",
+    title: 'Cuidado!',
     class: 'bg-danger-light border-danger',
     icon: 'ban',
     iconClass: 'text-danger',
@@ -35,29 +35,38 @@ const InformationBoxTypes: Record<
 const InformationBoxComponent = ({
   type = 'tips',
   subtitle,
-  
 }: InformationBoxProps) => {
-    return (
+  return (
+    <div
+      className={`InformationBox py-4 px-5 rounded w-full relative flex items-start mb-3 ${InformationBoxTypes[type].class}`}
+    >
       <div
-        className={`InformationBox py-4 px-5 rounded w-full relative flex items-start mb-3 ${InformationBoxTypes[type].class}`}
+        className={`w-6 h-6 flex justify-center items-center flex-shrink-0 rounded-full mr-3 bg-base-1`}
       >
-        <div className={`w-6 h-6 flex justify-center items-center flex-shrink-0 rounded-full mr-3 bg-base-1`}>
-          <Icon
-            icon={InformationBoxTypes[type].icon}
-            className={`${InformationBoxTypes[type].iconClass}`}
-            size={4} />
-        </div>
+        <Icon
+          icon={InformationBoxTypes[type].icon}
+          className={`${InformationBoxTypes[type].iconClass}`}
+          size={4}
+        />
+      </div>
 
-        <div className="flex-grow flex flex-col sm:flex-row items-start items-center justify-between min-w-0">
-          <div className="flex flex-col justify-center min-w-0 break-words tracking-4 leading-7 text-on-base">
-            <span className={`InformationBox-title text-f5 font-bold ${InformationBoxTypes[type].iconClass}`}>{InformationBoxTypes[type].title}</span>
-            {subtitle && <span className="InformationBox-subtitle text-f6 font-regular mt-1">{subtitle}</span>}
-            
-          </div>
+      <div className="flex-grow flex flex-col sm:flex-row items-start items-center justify-between min-w-0">
+        <div className="flex flex-col justify-center min-w-0 break-words tracking-4 leading-7 text-on-base">
+          <span
+            className={`InformationBox-title text-f5 font-bold ${InformationBoxTypes[type].iconClass}`}
+          >
+            {InformationBoxTypes[type].title}
+          </span>
+          {subtitle && (
+            <span className="InformationBox-subtitle text-f6 font-regular mt-1">
+              {subtitle}
+            </span>
+          )}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
 export const InformationBox = React.memo(InformationBoxComponent)
 
@@ -66,24 +75,24 @@ export interface InformationBoxProps {
    * @default info
    * */
   type?: keyof typeof InformationBoxTypes
-  /** 
+  /**
    * @default true
    */
   title: string | React.ReactNode
   /**
    * InformationBox text below title
    */
-  
+
   subtitle?: string | React.ReactNode
   /**
    * Space to extra content
    */
   content?: string | React.ReactNode
-   /**
-    * Content
-    */
+  /**
+   * Content
+   */
   hideButton?: boolean
-   /**
+  /**
     Hide Button
     */
 }
