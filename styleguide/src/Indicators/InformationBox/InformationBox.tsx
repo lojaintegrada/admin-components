ea69ea5def33f9c44ed2bf71e08f95edf7bb4396
Natanael Fernandes <natan-fernandes1@hotmail.com>
2022-09-05 16:44:26 -0300

    fix: pr requirements

--- styleguide/src/Indicators/InformationBox/InformationBox.tsx
+++ styleguide/src/Indicators/InformationBox/InformationBox.tsx
@@ -38,10 +38,10 @@ const InformationBoxComponent = ({
 }: InformationBoxProps) => {
   return (
     <div
-      className={`InformationBox py-4 px-5 rounded w-full relative flex items-start mb-3 ${InformationBoxTypes[type].class}`}
+      className={`InformationBox pl-4 pr-6 pt-5 pb-6 rounded w-full relative flex items-start mb-3 ${InformationBoxTypes[type].class}`}
     >
       <div
-        className={`w-6 h-6 flex justify-center items-center flex-shrink-0 rounded-full mr-3 bg-base-1`}
+        className={`w-10 h-10 flex justify-center items-center flex-shrink-0 rounded-full mr-3 bg-base-1`}
       >
         <Icon
           icon={InformationBoxTypes[type].icon}
@@ -51,7 +51,7 @@ const InformationBoxComponent = ({
       </div>
 
       <div className="flex-grow flex flex-col sm:flex-row items-start justify-between min-w-0">
-        <div className="flex flex-col justify-center min-w-0 break-words tracking-4 leading-7 text-on-base">
+        <div className="flex flex-col justify-center min-w-0 break-words tracking-4 leading-6 text-on-base">
           <span
             className={`InformationBox-title text-f5 font-bold ${InformationBoxTypes[type].iconClass}`}
           >
@@ -82,17 +82,5 @@ export interface InformationBoxProps {
   /**
    * InformationBox text below title
    */
-
   subtitle?: string | React.ReactNode
-  /**
-   * Space to extra content
-   */
-  content?: string | React.ReactNode
-  /**
-   * Content
-   */
-  hideButton?: boolean
-  /**
-    Hide Button
-    */
 }
