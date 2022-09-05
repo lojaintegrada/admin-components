05a7147b9523d0ea683be7e6420017dc2b88fae8
Larissa Santos <larissasantos.dev@gmail.com>
2022-09-05 14:54:46 -0300

    feat/add-devices-and-angle-icons

---
+++ styleguide/src/Icons/icons-path/Desktop.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Desktop = () => (
+    <path 
+        fill-rule="evenodd" 
+        clip-rule="evenodd" 
+        d="M16.5 11c0 .275-.225.5-.5.5H2a.501.501 0 0 1-.5-.5V2c0-.275.225-.5.5-.5h14c.275 0 .5.225.5.5v9Zm-6.009 5.5H7.509l1.05-3.5h.882l1.05 3.5ZM16 0H2a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h4.991l-1.05 3.5H2.5V18h13v-1.5h-3.441l-1.05-3.5H16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Z" 
+    />
+)
---
+++ styleguide/src/Icons/icons-path/Mobile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Mobile = () => (
+    <path 
+        fill-rule="evenodd" 
+        clip-rule="evenodd" 
+        d="M6 13a1 1 0 1 0 0 2 1 1 0 1 0 0-2ZM9.5 0h-7a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm0 1.5c.275 0 .5.225.5.5v14c0 .275-.225.5-.5.5h-7A.501.501 0 0 1 2 16V2c0-.275.225-.5.5-.5h7Z" 
+    />
+)
