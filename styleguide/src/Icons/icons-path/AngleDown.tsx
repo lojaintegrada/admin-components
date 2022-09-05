4fdd8c8fa609841e65aeedec2a40b2fa1252efee
Larissa Santos <larissasantos.dev@gmail.com>
2022-09-05 15:29:29 -0300

    Corrige erro no lint

--- styleguide/src/Icons/icons-path/AngleDown.tsx
+++ styleguide/src/Icons/icons-path/AngleDown.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export const AngleDown = () => (
-    <path 
-        fill-rule="evenodd" 
-        clip-rule="evenodd" 
-        d="M9 12.25c-.286 0-.558-.123-.748-.336l-4-4.5a1 1 0 0 1 1.495-1.329L9 9.745l3.252-3.66a1 1 0 1 1 1.495 1.33l-4 4.5a1 1 0 0 1-.747.334Z" 
-    />
+  <path
+    fillRule="evenodd"
+    d="M9 12.25c-.286 0-.558-.123-.748-.336l-4-4.5a1 1 0 0 1 1.495-1.329L9 9.745l3.252-3.66a1 1 0 1 1 1.495 1.33l-4 4.5a1 1 0 0 1-.747.334Z"
+    clipRule="evenodd"
+  />
 )
--- styleguide/src/Icons/icons-path/Plus.tsx
+++ styleguide/src/Icons/icons-path/Plus.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export const Plus = () => (
-    <path 
-        fill-rule="evenodd" 
-        clip-rule="evenodd" 
-        d="M16.5 7.75h-6.25V1.5h-2.5v6.25H1.5v2.5h6.25v6.25h2.5v-6.25h6.25v-2.5Z" 
-    />
+  <path
+    fillRule="evenodd"
+    d="M16.5 7.75h-6.25V1.5h-2.5v6.25H1.5v2.5h6.25v6.25h2.5v-6.25h6.25v-2.5Z"
+    clipRule="evenodd"
+  />
 )
