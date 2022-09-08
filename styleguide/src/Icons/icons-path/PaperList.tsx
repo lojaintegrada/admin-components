4d336070ae1d89d1982e51bc7ec7f811564660c3
Gabriel Azevedo <gabrielx86@live.com>
2022-09-08 12:31:02 -0300

    chore: lint fix

--- styleguide/src/Icons/icons-path/PaperList.tsx
+++ styleguide/src/Icons/icons-path/PaperList.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const PaperList = () => (
   <path
-    fill-rule="evenodd"
+    fillRule="evenodd"
     d="M17 3c0-1.657-1.343-3-3-3H4C2.343 0 1 1.343 1 3v12c0 1.657 1.343 3 3 3h10c1.657 0 3-1.343 3-3V3Zm-3 13.5c.827 0 1.5-.673 1.5-1.5V3c0-.827-.673-1.5-1.5-1.5H4c-.827 0-1.5.673-1.5 1.5v12c0 .827.673 1.5 1.5 1.5h10ZM5 5h8V4H5v1Zm0 3h8V7H5v1Zm0 3h8v-1H5v1Zm0 3h8v-1H5v1Z"
-    clip-rule="evenodd"
+    clipRule="evenodd"
   />
 )
