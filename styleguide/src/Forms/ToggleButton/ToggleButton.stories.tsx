6d6b9a541a490cd82c3579be25e9678dfb85e427
Larissa Santos <86785228+itslarissayall@users.noreply.github.com>
2022-09-09 10:56:14 -0300

    Feat/add toggle button (#146)

---
+++ styleguide/src/Forms/ToggleButton/ToggleButton.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import { Icon } from '../../Icons/Icon'
+import { ToggleButton, ToggleButtonProps } from '.'
+
+export default {
+  title: 'Forms/ToggleButton',
+  component: ToggleButton,
+} as Meta
+
+const Template: Story<ToggleButtonProps> = (args) => <ToggleButton {...args} />
+
+export const WithIcon = Template.bind({})
+WithIcon.args = {  
+  disabled: false,
+  children: [
+    <Icon icon='desktop' key={1} />,
+    <Icon icon='mobile' key={2}/>,
+  ]
+}
+
+export const WithText = Template.bind({})
+WithText.args = {  
+  disabled: false,
+  children: [ 
+    <span key={1}>With Text</span>,
+    <span key={2}>With Text</span>,
+    <span key={3}>With Text</span>,
+  ]
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {  
+  disabled: true,
+  children: [ 
+    <span key={1}>Disabled</span>,
+    <span key={2}>Disabled</span>,
+  ]
+}
+
+export const WithOnChange = Template.bind({})
+WithOnChange.args = {
+  disabled: false,
+  children: [
+    <Icon icon='desktop' key={1} />,
+    <Icon icon='mobile' key={2}/>,
+  ],
+  onChange: (e) => {
+    console.log(e.target.name)
+  },
+}
---
+++ styleguide/src/Forms/ToggleButton/ToggleButton.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+
+const ToggleButtonComponent = (
+  {
+    value,
+    className = '',
+    id,
+    children,
+    disabled,
+    defaultCheckedIndex = 0,
+    onChange,
+    ...props
+  }: ToggleButtonProps,
+  ref: React.ForwardedRef<HTMLInputElement>
+) => {
+  const toggleClasses = disabled
+    ? `pointer-events-none bg-base-3 border-card-stroke text-on-base-2`
+    : `peer-checked:bg-primary peer-checked:border-primary peer-checked:text-base-1 text-primary`
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    onChange && onChange(e)
+  }
+
+  const toggleItems = children.map((item, index) => {
+    const borderRadius =
+      index === 0
+        ? 'rounded-l'
+        : children.length - 1 === index
+        ? 'border-r rounded-r'
+        : ''
+    return (
+      <li
+        key={`toggle-btn-[${index}]`}
+        className="inline-flex justify-center items-center h-12"
+      >
+        <label className="relative inline-flex justify-center items-center h-12">
+          <input
+            ref={ref}
+            className="peer absolute top-0 bottom-0 left-0 right-0 cursor-pointer opacity-0 z-20"
+            type="radio"
+            id={id}
+            name="toggleButton"
+            value={value}
+            onChange={handleChange}
+            {...props}
+            defaultChecked={defaultCheckedIndex === index ? true : false}
+            disabled={disabled}
+          />
+          <div
+            className={`${className} checkmark ${toggleClasses} inline-flex justify-center items-center ${borderRadius} py-4 px-6 border-y border-l h-12`}
+          >
+            {item}
+          </div>
+        </label>
+      </li>
+    )
+  })
+
+  return (
+    <div
+      className={`${className} inline-flex justify-center items-center h-12 ${toggleClasses}} rounded text-f6 tracking-4 font-semibold`}
+    >
+      <ul className="list-none inline-flex justify-center items-center h-12">
+        {toggleItems}
+      </ul>
+    </div>
+  )
+}
+
+const ToggleButtonWithForwardRef = React.forwardRef(ToggleButtonComponent)
+export const ToggleButton = React.memo(ToggleButtonWithForwardRef)
+
+export interface ToggleButtonProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  /**
+   * Custom class name
+   * */
+  className?: string
+  /**
+   * Custom id
+   * */
+  id?: string
+  /**
+   * Custom value
+   * */
+  value?: string | number
+  /**
+   * Set icon or an text element to be a children
+   * */
+  children: React.ReactNode[]
+  /**
+   * Set element should be a checked
+   * */
+  defaultCheckedIndex: number
+}
