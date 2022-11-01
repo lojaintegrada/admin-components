2bf39388aba75d76566ccecf27249be5482d722a
brunaazambuja <bubu1azambuja@gmail.com>
2022-11-01 18:10:14 -0300

    Resolving jest test.

--- styleguide/test/blah.test.tsx
+++ styleguide/test/blah.test.tsx
@@ -2,8 +2,7 @@
 // import * as ReactDOM from 'react-dom'
 // import { Default as Thing } from '../src/Components/Button/Button.stories'
 
-const specTitle = require('cypress-sonarqube-reporter/specTitle')
-describe(specTitle('Thing'), () => {
+describe('Thing', () => {
   it('renders without crashing', () => {
     // const div = document.createElement('div')
     // ReactDOM.render(<Thing />, div)
