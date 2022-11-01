c680b2fea850a33855ecf0f0c8457b9bd5f40789
brunaazambuja <bubu1azambuja@gmail.com>
2022-11-01 15:06:15 -0300

    Removing ; to linter

--- styleguide/test/blah.test.tsx
+++ styleguide/test/blah.test.tsx
@@ -2,7 +2,7 @@
 // import * as ReactDOM from 'react-dom'
 // import { Default as Thing } from '../src/Components/Button/Button.stories'
 
-const specTitle = require('cypress-sonarqube-reporter/specTitle');
+const specTitle = require('cypress-sonarqube-reporter/specTitle')
 describe(specTitle('Thing'), () => {
   it('renders without crashing', () => {
     // const div = document.createElement('div')
