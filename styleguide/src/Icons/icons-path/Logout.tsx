3f7b760b22a07483cd57ae4c24681a761293f5a6
Flávio Fiori <flavio.fiori31@gmail.com>
2023-03-06 10:43:14 -0300

    :sparkles: adds Logout icon

---
+++ styleguide/src/Icons/icons-path/Logout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Logout = () => (
+  <path
+    fillRule="evenodd"
+    d="m17.781 8.43-4.5-5a.85.85 0 0 0-1.264 1.139l3.314 3.68H5.15v1.5h10.181l-3.314 3.682a.852.852 0 0 0 1.264 1.138l4.5-5a.85.85 0 0 0 0-1.138ZM8 18H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6v1.5H2a.5.5 0 0 0-.5.5v14c0 .277.223.5.5.5h6V18Z"
+    clipRule="evenodd"
+  />
+)
