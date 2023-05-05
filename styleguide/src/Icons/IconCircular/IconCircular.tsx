4f60baa31de6bf93093d6f64838dc64023b88681
gabrielzevedo <gabrielx86@live.com>
2023-05-05 12:32:36 -0300

    chore: lint

--- styleguide/src/Icons/IconCircular/IconCircular.tsx
+++ styleguide/src/Icons/IconCircular/IconCircular.tsx
@@ -8,29 +8,41 @@ const variantsStyles: {
     icon: IconProps['icon']
   }
 } = {
-  'success': {
+  success: {
     bgClass: 'bg-primary',
     icon: 'check',
   },
-  'warning': {
+  warning: {
     bgClass: 'bg-warning',
     icon: 'exclamation',
   },
-  'danger': {
+  danger: {
     bgClass: 'bg-danger',
     icon: 'times',
   },
 }
 
 export const IconCircular: FunctionComponent<IconCircularProps> = React.memo(
-  ({ variant = 'success', icon: customIcon, bgClass: customBgClass }: IconCircularProps) => {
-    const icon = customIcon ? customIcon : (variantsStyles[variant].icon || 'check')
-    const bgClass = customBgClass ? customBgClass : (variantsStyles[variant].bgClass || 'bg-primary')
+  ({
+    variant = 'success',
+    icon: customIcon,
+    bgClass: customBgClass,
+  }: IconCircularProps) => {
+    const icon = customIcon
+      ? customIcon
+      : variantsStyles[variant].icon || 'check'
+    const bgClass = customBgClass
+      ? customBgClass
+      : variantsStyles[variant].bgClass || 'bg-primary'
 
     return (
       <>
-        <div className={`icon-circular flex w-20 h-20 mx-auto rounded-full items-center justify-center bg-opacity-20 ${bgClass}`}>
-          <div className={`flex w-14 h-14 rounded-full items-center justify-center ${bgClass}`}>
+        <div
+          className={`icon-circular flex w-20 h-20 mx-auto rounded-full items-center justify-center bg-opacity-20 ${bgClass}`}
+        >
+          <div
+            className={`flex w-14 h-14 rounded-full items-center justify-center ${bgClass}`}
+          >
             <Icon icon={icon} className="text-base-1" size={7} block />
           </div>
         </div>
--- styleguide/src/Icons/icons-path/Exclamation.tsx
+++ styleguide/src/Icons/icons-path/Exclamation.tsx
@@ -7,9 +7,7 @@ export const Exclamation = () => (
       d="M3 0a3 3 0 0 1 3 3v14a3 3 0 1 1-6 0V3a3 3 0 0 1 3-3Z"
       clipRule="evenodd"
     />
-    <path
-      d="M6 27a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-    />
+    <path d="M6 27a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
     <path
       fillRule="evenodd"
       d="M3 29a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
