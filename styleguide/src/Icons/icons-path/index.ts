a5631d483910039cd9e970228bce1ed9caa62731
Flávio Fiori <flavio.fiori31@gmail.com>
2023-06-01 15:36:07 -0300

    :sparkles: adiciona novos icones

--- styleguide/src/Icons/icons-path/index.ts
+++ styleguide/src/Icons/icons-path/index.ts
@@ -67,6 +67,8 @@ import { Camera } from './Camera'
 import { Nav } from './Nav'
 import { ShoppingCart } from './ShoppingCart'
 import { WhatsApp } from './WhatsApp'
+import { IconSolidLI } from './IconSolidLI'
+import { ThinAngleRight } from './ThinAngleRight'
 
 export const icons = {
   adjust: Adjust,
@@ -139,4 +141,6 @@ export const icons = {
   sync: Sync,
   user: User,
   whatsapp: WhatsApp,
+  iconSolidLI: IconSolidLI,
+  thinAngleRight: ThinAngleRight,
 }
