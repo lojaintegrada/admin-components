2f4ec496444573468f41ff96c7d6cb4e83b12ea2
Gabriel Azevedo <gabrielx86@live.com>
2024-04-12 15:11:54 -0300

    fix: adjust InformationBox (#207)
    * fix: adjust InformationBox type and change colors
    
    * fix: fix colors
    
    * chore: adjust test

--- styleguide/src/Indicators/InformationBox/InformationBox.spec.tsx
+++ styleguide/src/Indicators/InformationBox/InformationBox.spec.tsx
@@ -5,28 +5,27 @@ import * as stories from './InformationBoxt.stories'
 
 const { Default } = composeStories(stories)
 
-const specTitle = require('cypress-sonarqube-reporter/specTitle');
+const specTitle = require('cypress-sonarqube-reporter/specTitle')
 describe(specTitle('InformationBox tests'), () => {
   it('Default', () => {
     mount(<Default />)
-    cy.get('.InformationBox-title').last().contains('Dica!')
+    cy.get('.InformationBox-title').last().contains('Informação')
   })
 
   it('Variants', () => {
-    mount(<Default type="tip" />)
-    cy.get('.InformationBox-icon').should('have.class', 'icon-lightbulb')
-    cy.get('.InformationBox').should('have.class', 'bg-success-light')
-
     mount(<Default type="warning" />)
-    cy.get('.InformationBox-icon').should('have.class', 'icon-exclamationTriangle')
+    cy.get('.InformationBox-icon').should('have.class', 'icon-infoCircle')
     cy.get('.InformationBox').should('have.class', 'bg-warning-light')
 
     mount(<Default type="danger" />)
-    cy.get('.InformationBox-icon').should('have.class', 'icon-ban')
+    cy.get('.InformationBox-icon').should(
+      'have.class',
+      'icon-exclamationTriangle'
+    )
     cy.get('.InformationBox').should('have.class', 'bg-danger-light')
-    
-    mount(<Default type="info" />)
-    cy.get('.InformationBox-icon').should('have.class', 'icon-infoCircle')
-    cy.get('.InformationBox').should('have.class', 'bg-focus-light')
+
+    mount(<Default type="success" />)
+    cy.get('.InformationBox-icon').should('have.class', 'icon-check')
+    cy.get('.InformationBox').should('have.class', 'bg-success-light')
   })
 })
