267023f388d54732d8540eb6c1ad1c40ea85a8e5
HCarrer <hmcarrer@gmail.com>
2024-05-24 15:45:15 -0300

    default periods as constants

--- styleguide/src/Forms/Calendar/constants.ts
+++ styleguide/src/Forms/Calendar/constants.ts
@@ -1,25 +1,30 @@
-export const defaultPeriods = [
-  {
-    id: 'ontem',
-    label: 'Ontem',
-    value: 1
-  },
-  {
-    id: '7-dias',
-    label: '7 dias',
-    value: 7
-  },
-  {
-    id: '30-dias',
-    label: '30 dias',
-    value: 30
-  },
-  {
-    id: 'selecionar-periodo',
-    label: 'Selecionar período',
-    icon: 'calendarAlt'
-  }
-]
+import { PeriodsType } from "."
+
+export const YESTERDAY: PeriodsType = {
+  id: 'ontem',
+  label: 'Ontem',
+  value: 1
+}
+
+export const PREVIOUS_SEVEN_DAYS: PeriodsType = {
+  id: '7-dias',
+  label: '7 dias',
+  value: 7
+}
+
+export const PREVIOUS_THIRTY_DAYS: PeriodsType = {
+  id: '30-dias',
+  label: '30 dias',
+  value: 30
+}
+
+export const CUSTOM_PERIOD: PeriodsType = {
+  id: 'selecionar-periodo',
+  label: 'Selecionar período',
+  icon: 'calendarAlt'
+}
+
+export const defaultPeriods: PeriodsType[] = [YESTERDAY, PREVIOUS_SEVEN_DAYS, PREVIOUS_THIRTY_DAYS, CUSTOM_PERIOD]
 
 export const months = [
   'Janeiro',
