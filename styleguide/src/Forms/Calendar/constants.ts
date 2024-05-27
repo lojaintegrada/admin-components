bb5025650fc6eed3111b86c729b17bd9e6fc9946
HCarrer <hmcarrer@gmail.com>
2024-05-27 10:29:55 -0300

    side files lint

--- styleguide/src/Forms/Calendar/constants.ts
+++ styleguide/src/Forms/Calendar/constants.ts
@@ -1,30 +1,35 @@
-import { PeriodsType } from "."
+import { PeriodsType } from '.'
 
 export const YESTERDAY: PeriodsType = {
   id: 'ontem',
   label: 'Ontem',
-  value: 1
+  value: 1,
 }
 
 export const PREVIOUS_SEVEN_DAYS: PeriodsType = {
   id: '7-dias',
   label: '7 dias',
-  value: 7
+  value: 7,
 }
 
 export const PREVIOUS_THIRTY_DAYS: PeriodsType = {
   id: '30-dias',
   label: '30 dias',
-  value: 30
+  value: 30,
 }
 
 export const CUSTOM_PERIOD: PeriodsType = {
   id: 'selecionar-periodo',
   label: 'Selecionar período',
-  icon: 'calendarAlt'
+  icon: 'calendarAlt',
 }
 
-export const defaultPeriods: PeriodsType[] = [YESTERDAY, PREVIOUS_SEVEN_DAYS, PREVIOUS_THIRTY_DAYS, CUSTOM_PERIOD]
+export const defaultPeriods: PeriodsType[] = [
+  YESTERDAY,
+  PREVIOUS_SEVEN_DAYS,
+  PREVIOUS_THIRTY_DAYS,
+  CUSTOM_PERIOD,
+]
 
 export const months = [
   'Janeiro',
--- styleguide/src/Forms/Calendar/helper.tsx
+++ styleguide/src/Forms/Calendar/helper.tsx
@@ -1,32 +1,44 @@
-import { months } from "./constants"
+import { months } from './constants'
 
-export const getDayClassName = (day: Date, dayIsEnabled: boolean, startDate: Date, endDate: Date, period: 'start' | 'end'): string => {
-  const _day = day.setHours(0,0,0,0)
-  const _startDate = startDate.setHours(0,0,0,0)
-  const _endDate = endDate.setHours(0,0,0,0)
-  var classname = ''
-  if(period == 'start') {
-    if (_day == _startDate) classname = '!bg-primary !rounded-l !rounded-r !text-base-1 !font-bold before:left-5'
+export const getDayClassName = (
+  day: Date,
+  dayIsEnabled: boolean,
+  startDate: Date,
+  endDate: Date,
+  period: 'start' | 'end'
+): string => {
+  const _day = day.setHours(0, 0, 0, 0)
+  const _startDate = startDate.setHours(0, 0, 0, 0)
+  const _endDate = endDate.setHours(0, 0, 0, 0)
+  let classname = ''
+  if (period === 'start') {
+    if (_day === _startDate)
+      classname =
+        '!bg-primary !rounded-l !rounded-r !text-base-1 !font-bold before:left-5'
   }
-  if(period == 'end') {
-    if (_day == _endDate) classname = '!bg-primary !rounded-l !rounded-r !text-base-1 !font-bold before:right-5'
+  if (period === 'end') {
+    if (_day === _endDate)
+      classname =
+        '!bg-primary !rounded-l !rounded-r !text-base-1 !font-bold before:right-5'
   }
-  if(dayIsEnabled) {
+  if (dayIsEnabled) {
     classname = 'hover:!text-base-1 hover:font-bold'
-    if(_day < _startDate || _day > _endDate) classname = '!bg-base-1 hover:!rounded-l hover:!rounded-r hover:!bg-primary hover:!text-base-1 hover:!font-bold'
+    if (_day < _startDate || _day > _endDate)
+      classname =
+        '!bg-base-1 hover:!rounded-l hover:!rounded-r hover:!bg-primary hover:!text-base-1 hover:!font-bold'
   }
   return classname
 }
 
 export const getMonthName = (month: number) => {
-  return months[month].slice(0,3).toUpperCase()
-};
+  return months[month].slice(0, 3).toUpperCase()
+}
 
 export const formatDate = (startDate: Date, endDate: Date) => {
   const _startDate = startDate.getDate()
   const _endDate = endDate.getDate()
   const startMonth = months[startDate.getMonth()]
   const endMonth = months[endDate.getMonth()]
-  if(_startDate === _endDate) return `${_startDate} de ${startMonth} `
+  if (_startDate === _endDate) return `${_startDate} de ${startMonth} `
   return `${_startDate} de ${startMonth} - ${_endDate} de ${endMonth}`
-};
+}
