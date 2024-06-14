6fc63116a0cd3ce4c20e320bdaa851e4a42b3946
Matheus Alves <web.matheusalves@gmail.com>
2024-06-14 11:32:46 -0300

    feat: add icon table (#219)

---
+++ styleguide/src/Icons/icons-path/Table.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Table = () => (
+  <path
+    fillRule="evenodd"
+    clipRule="evenodd"
+    d="M16 15.5H9.75V9.75h6.75V15c0 .275-.225.5-.5.5Zm-14 0a.501.501 0 0 1-.5-.5V9.75h6.75v5.75H2Zm0-13h6.25v5.75H1.5V3c0-.275.225-.5.5-.5Zm14 0c.275 0 .5.225.5.5v5.25H9.75V2.5H16ZM16 1H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Z"
+  />
+)
